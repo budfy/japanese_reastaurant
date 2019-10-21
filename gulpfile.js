1328be1ef6eaea2ed3350fde3a8845f985178fe3
budfy <serdyuk.ro@gmail.com>
2019-10-21 12:42:26 +0300

    Gulp installation

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+let gulp = require ('gulp'),
+    sass = require('gulp-sass'),
+    rename = require('gulp-rename'),
+    browserSync = require('browser-sync'),
+    imagemin = require('gulp-imagemin'),
+    pngmin = require('imagemin-pngquant'),
+    cssmin = require('gulp-cssmin'),
+    autoprefixer = require('gulp-autoprefixer'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat'),
+    htmlimport = require('gulp-html-import'),
+    del = require('del')
+
+   
